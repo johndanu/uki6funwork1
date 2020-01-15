f9728b44cf4de68308c18491ea7a2585c875fee9
johndanu <johnasfone@gmail.com>
2020-01-15 10:45:53 +0530

    english 3

--- main.js
+++ main.js
@@ -14,29 +14,28 @@ $(document).ready(function() {
         });
   });
 
-  SC.stream('/tracks/299479465',function(sound){
-    $('#start').click(function(e) {
+  SC.stream('/tracks/105827731',function(sound){
+    $('#start-e2').click(function(e) {
            e.preventDefault();
            sound.start();
          });
-    $('#stop').click(function(e) {
+    $('#stop-e2').click(function(e) {
            e.preventDefault();
            sound.stop();
         });
   });
 
-  SC.stream('/tracks/271004119',function(sound){
-    $('#start').click(function(e) {
+  SC.stream('/tracks/285134266',function(sound){
+    $('#start-e3').click(function(e) {
            e.preventDefault();
            sound.start();
          });
-    $('#stop').click(function(e) {
+    $('#stop-e3').click(function(e) {
            e.preventDefault();
            sound.stop();
         });
-
-
-  });  SC.stream('/tracks/271004119',function(sound){
+  });
+  SC.stream('/tracks/271004119',function(sound){
       $('#start').click(function(e) {
              e.preventDefault();
              sound.start();
